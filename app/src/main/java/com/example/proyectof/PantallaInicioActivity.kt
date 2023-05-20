cf5d3aa9a1b65bd29a047ec875c790c18b8e60e2
Fernando Itzama <fernandonovca@gmail.com>
2023-05-20 13:32:13 -0600

    prueba 2

--- app/src/main/java/com/example/proyectof/PantallaInicioActivity.kt
+++ app/src/main/java/com/example/proyectof/PantallaInicioActivity.kt
@@ -37,7 +37,7 @@ class PantallaInicioActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_manejo_cuentas, R.id.nav_movimientos, R.id.nav_reportes
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
