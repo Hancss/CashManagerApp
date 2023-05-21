3b6e58c77862246ce6997d11e74381210922916a
Fernando Itzama <fernandonovca@gmail.com>
2023-05-21 17:15:20 -0600

    Agregado Menu con un bug de seleccion

--- app/src/main/java/com/example/proyectof/PantallaInicioActivity.kt
+++ app/src/main/java/com/example/proyectof/PantallaInicioActivity.kt
@@ -1,7 +1,10 @@
 package com.example.proyectof
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.ExpandableListView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,12 +14,23 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.example.proyectof.databinding.ActivityPantallaInicioBinding
+import com.example.proyectof.ui.expanded.ExpandedMenuAdapter
+import com.example.proyectof.ui.expanded.ExpandedMenuModel
+import java.util.ArrayList
+import java.util.HashMap
 
 class PantallaInicioActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityPantallaInicioBinding
+
+    private var viewGroup: View? = null
+
+    private val headerList: ArrayList<ExpandedMenuModel> = ArrayList()
+    private val childList: HashMap<ExpandedMenuModel, ArrayList<String>> =
+        HashMap()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,11 +51,136 @@ class PantallaInicioActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_manejo_cuentas, R.id.nav_movimientos, R.id.nav_reportes
+                R.id.nav_home,
+                R.id.nav_manejo_cuentas,
+                R.id.nav_movimientos,
+                R.id.nav_reportes,
+                R.id.nav_firstChild,
+                R.id.nav_secondChild,
+                R.id.nav_thirdChild,
+                R.id.nav_fourthChild,
+                R.id.nav_fifthChild
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        prepareListData()
+
+        //Initialize and Assign ExpandableListView
+        val expandableListView: ExpandableListView = binding.expandedListView
+
+
+        //Set Adapter in ExpandableListView :
+        val mMenuAdapter = ExpandedMenuAdapter(this, headerList, childList, expandableListView)
+        expandableListView.setAdapter(mMenuAdapter)
+
+        expandableListView.choiceMode = ExpandableListView.CHOICE_MODE_SINGLE
+
+        expandableListView.setOnGroupClickListener { parent, _, groupPosition, _ ->
+            when (groupPosition) {
+                0 -> {
+                    if (parent.isGroupExpanded(groupPosition)) {
+                        parent.collapseGroup(groupPosition)
+                        parent.collapseGroup(1)
+                        parent.collapseGroup(2)
+                        parent.collapseGroup(3)
+                    } else {
+                        parent.expandGroup(groupPosition)
+                        parent.collapseGroup(1)
+                        parent.collapseGroup(2)
+                        parent.collapseGroup(3)
+                    }
+                }
+
+                1 -> {
+                    if (parent.isGroupExpanded(groupPosition)) {
+                        parent.collapseGroup(groupPosition)
+                        parent.collapseGroup(0)
+                        parent.collapseGroup(2)
+                        parent.collapseGroup(3)
+                    } else {
+                        parent.expandGroup(groupPosition)
+                        parent.collapseGroup(0)
+                        parent.collapseGroup(2)
+                        parent.collapseGroup(3)
+                    }
+                }
+
+                2 -> {
+                    if (parent.isGroupExpanded(groupPosition)) {
+                        parent.collapseGroup(groupPosition)
+                        parent.collapseGroup(0)
+                        parent.collapseGroup(1)
+                        parent.collapseGroup(3)
+                    } else {
+                        parent.expandGroup(groupPosition)
+                        parent.collapseGroup(0)
+                        parent.collapseGroup(1)
+                        parent.collapseGroup(3)
+                    }
+                }
+
+                3 -> {
+                    if (parent.isGroupExpanded(groupPosition)) {
+                        parent.collapseGroup(groupPosition)
+                        parent.collapseGroup(0)
+                        parent.collapseGroup(1)
+                        parent.collapseGroup(2)
+                    } else {
+                        parent.expandGroup(groupPosition)
+                        parent.collapseGroup(0)
+                        parent.collapseGroup(1)
+                        parent.collapseGroup(2)
+                    }
+                }
+            }
+            true
+        }
+
+        expandableListView.setOnChildClickListener { parent, view, groupPosition, childPosition, _ ->
+            view.isSelected = true
+            viewGroup?.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            viewGroup = view
+            viewGroup?.setBackgroundColor(Color.parseColor("#2ba89c"))
+            drawerLayout.closeDrawer(GravityCompat.START)
+
+            when (groupPosition) {
+                0 -> { // Menu Inicio
+                    when (childPosition) {
+                        0 -> navController.navigate(R.id.nav_firstChild)
+                    }
+                }
+
+                1 -> {  //Menu Movimientos
+                    when (childPosition) {
+                        0 -> navController.navigate(R.id.nav_secondChild)
+                        1 -> navController.navigate(R.id.nav_thirdChild)
+                        2 -> navController.navigate(R.id.nav_fourthChild)
+                    }
+                }
+
+                2 -> {  //Menu Manejo de Cuentas
+                    when (childPosition) {
+                        0 -> navController.navigate(R.id.nav_fifthChild)
+                        1 -> navController.navigate(R.id.nav_fifthChild)
+                        2 -> navController.navigate(R.id.nav_fifthChild)
+                        3 -> navController.navigate(R.id.nav_fifthChild)
+
+                    }
+                }
+
+                3 -> {  //Menu Reportes
+                    when (childPosition) {
+                        0 -> navController.navigate(R.id.nav_fifthChild)
+                        1 -> navController.navigate(R.id.nav_fifthChild)
+                        2 -> navController.navigate(R.id.nav_fifthChild)
+                    }
+                }
+            }
+
+            true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -53,5 +192,52 @@ class PantallaInicioActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_pantalla_inicio)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun prepareListData() {
+
+        val headerInicio = ExpandedMenuModel()
+        headerInicio.itemName = "Inicio"
+        headerInicio.itemIcon = R.drawable.ic_menu_home
+        headerList.add(headerInicio)
+
+        val headerMovimientos = ExpandedMenuModel()
+        headerMovimientos.itemName = "Movimientos"
+        headerMovimientos.itemIcon = R.drawable.ic_menu_movimientos
+        headerList.add(headerMovimientos)
+
+        val headerManejoCuentas = ExpandedMenuModel()
+        headerManejoCuentas.itemName = "Manejo de Cuentas"
+        headerManejoCuentas.itemIcon = R.drawable.ic_menu_manejo_cuentas
+        headerList.add(headerManejoCuentas)
+
+        val headerReportes = ExpandedMenuModel()
+        headerReportes.itemName = "Reportes"
+        headerReportes.itemIcon = R.drawable.ic_menu_reportes
+        headerList.add(headerReportes)
+
+        val childInicio = ArrayList<String>()
+        childInicio.add("Inicio")
+        childList[headerInicio] = childInicio
+
+        val childMovimientos = ArrayList<String>()
+        childMovimientos.add("Ingreso")
+        childMovimientos.add("Gasto")
+        childMovimientos.add("Transferencias")
+        childList[headerMovimientos] = childMovimientos
+
+        val childManejoCuentas = ArrayList<String>()
+        childManejoCuentas.add("Consultar Movimientos")
+        childManejoCuentas.add("Agregar Cuenta")
+        childManejoCuentas.add("Cambiar Nombre de la Cuenta")
+        childManejoCuentas.add("Eliminar una Cuenta")
+        childList[headerManejoCuentas] = childManejoCuentas
+
+        val childReportes = ArrayList<String>()
+        childReportes.add("Resumen Mensual")
+        childReportes.add("Últimos Movimientos")
+        childReportes.add("Categorías")
+        childList[headerReportes] = childReportes
+
     }
 }
---
+++ app/src/main/java/com/example/proyectof/ui/expanded/ExpandedMenuModel.kt
@@ -0,0 +1,6 @@
+package com.example.proyectof.ui.expanded
+
+class ExpandedMenuModel {
+    var itemName = ""
+    var itemIcon = -1
+}
